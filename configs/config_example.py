8db5eb109e4dc3c5ced4eb85bf0b8c75c1fdbd27
Luca Cantarello <dalc@Lucas-MBP.fritz.box>
2022-09-18 23:14:59 +0200

    New configuration file added, all others removed and modified offlineQ.py script

---
+++ configs/config_example.py
@@ -0,0 +1,107 @@
+####################################################################
+##  FILE CONTAINING CONFIGURATION PARAMENTERS            	  ##
+####################################################################
+'''
+List of fixed parameters for model integration and EnKF.
+'''
+
+import numpy as np
+from init_cond_modRSW import init_cond_topog_cos
+from relax_sol_modRSW import U_relax_0
+
+'''Output directory'''
+outdir = 'output/config_example'
+
+''' MODEL PARAMETERS '''
+
+Neq = 3     # number of equations in system (3 with topography, 4 with rotation)
+L = 1.0     # length of domain (non-dimensional)
+
+Nk_fc = 200                         # forecast resolution
+dres = 2                            # refinement factor for truth gridsize
+Nk_tr = dres*Nk_fc                  # truth resolution
+n_d = Neq * Nk_fc                   # total number of variables (dgs of freedom)
+
+cfl_fc = 0.5 # Courant Friedrichs Lewy number for time stepping (forecast)
+cfl_tr = 0.5 # Courant Friedrichs Lewy number for time stepping (truth)
+
+Ro = 'Inf'          # Rossby number, i.e. Ro ~ V0/(f*L0)
+Fr = 1.1            # Froude number, i.e. Fr ~ V0/sqrt(gH)
+g = Fr**(-2) 	# effective gravity, determined by scaling.
+A = 0.1
+V = 1.
+
+# RELAXATION SOLUTION FROM relax_sol_ismodRSW.py:
+U_relax = U_relax_0
+tau_rel = 1.
+
+### INITIAL CONDITION ### 
+sig_ic = [0.1,0.05,0.0]                 # initial perturbation (st. dev) to generate initial conditions [h,u,r]
+ic = init_cond_topog_cos 
+
+# FLUID THRESHOLDS
+H0 = 1.0
+Hc = 1.02 # convection fluid threshold
+Hr = 1.05 # rain fluid threshold
+
+# RAIN AND CONVECTION PARAMETERS
+beta = 0.2 # coefficient controlling rain production
+alpha2 = 10 # coefficient controlling rain removal
+cc2 = 0.1*g*Hr # coefficient controlling suppressing of convection
+
+''' FILTER PARAMETERS '''
+dtmeasure = 0.144  # duration of forecast step in non-dimensional time units [1h = 1/((L/U)/3600) if U in m/s and L in m]
+n_ens = 18                              # number of ensemble members
+TIMEOUT_PAR = n_ens*4			# time to wait until all forecasts running in parallel are over
+Nmeas = 48                              # number of cycles
+Nforec = 13				#  duration of the *dtmeasure*-long forecast launched at the end of each analysis step
+NIAU = 1000				# suppress injection of additional inflation with IAU for the first NIAU hours since assimilation 
+tn = 0.0                                # initial time
+spin_up = 12  # spin-up duration for plotting in *dtmeasure*: data during this period are discarded
+tmax = (Nmeas+Nforec)*dtmeasure   # maximum duration of truth trajectory, from initial condition to end of last forecast
+t_end_assim = Nmeas*dtmeasure   # duration of DA in non-dimensional time units
+tmeasure = dtmeasure
+assim_time = np.linspace(tn,t_end_assim,Nmeas+1) # array of times of analysis steps
+lead_times = [0,3,6,9,12]  # lead times for plotting
+ass_freq='1h'   # frequency of assimilation. This is only a string for file names and need to be set together with dtmeasure
+
+### OBSERVING SYSTEM PARAMETERS ###
+ob_noise = [0.05,0.02,0.003]            # ob noise for [h,u,r]
+obs_h_d = 25 # observation spacing for h (in grid points)
+obs_u_d = 20 # observation spacing for u (in grid points)
+obs_r_d = 20 # observation spacing for r (in grid points)
+n_obs_h = Nk_fc // obs_h_d # number of h observations
+n_obs_u = Nk_fc // obs_u_d # number of u observations
+n_obs_r = Nk_fc // obs_r_d # number of r observations
+n_obs = n_obs_h + n_obs_u + n_obs_r # total number of observations
+# Mask for u observations in the observation vector
+h_obs_mask = np.array(list(range(n_obs_h)))
+# Mask for r observations in the observation vector
+hu_obs_mask = np.array(list(range(n_obs_h,n_obs_h+n_obs_u)))
+# Mask for r observations in the observation vector
+hr_obs_mask = np.array(list(range(n_obs_h+n_obs_u,n_obs_h+n_obs_u+n_obs_r)))
+
+
+### Q MATRIX GENERATION ###
+model_noise = [0.0,0.0,0.0]     # user-specified model noise to create model error covariance matrix Q 
+Nhr = 1                             # Duration of model forecasts used to compute Q
+Q_FUNC = 'Q_nhr()'                  # Q_predef(): user-specified error covariance matrix Q, Q_nhr(): error covariance matrix Q computed online
+rMODNOISE = 0                       # binary parameter to control the presence of additive inflation in r (1=yes,0=no)
+hMODNOISE = 1                    # binary parameter to control the presence of additive inflation in sigma (1=yes,0=no)
+
+### OUTER LOOP PARAMETERS (main_p.py) ###
+Parameters for outer loop are specified in main_p.py 
+loc     : localisation scale
+add_inf : additive infaltaion factor
+rtpp    : Relaxation to Prior Perturbations scaling factor
+rtps    : Relaxation to Prior Spread scaling factor
+'''
+# MUST BE FLOATING POINT
+loc = [ 0.5, 1.0, 1.5, 2.0 ]
+add_inf = [0.05, 0.08, 0.1, 0.12, 0.15, 0.2, 0.3, 0.4, 0.5 ]
+rtpp = [0.5]
+rtps = [ 0.1, 0.3, 0.5, 0.7, 0.9 ]
+
+##################################################################
+#			END OF PROGRAM				 #
+##################################################################
--- offlineQ.py
+++ offlineQ.py
@@ -1,18 +1,18 @@
 #######################################################################
-# Investigating computation and structure of model error and candidate Q matrices
-#######################################################################
+### This script generates a Q matrix according to the parameters in the configuration file
+
+##################################################################
+# GENERIC MODULES REQUIRED
+##################################################################
 import numpy as np
 import importlib
 import sys
-import matplotlib.pyplot as plt
-#from parameters import *
 from scipy import linalg
 from f_modRSW import make_grid, step_forward_topog, time_step, step_forward_modRSW
 
 ##################################################################
 # IMPORT PARAMETERS FROM CONFIGURATION FILE
 ##################################################################
-
 spec = importlib.util.spec_from_file_location("config", sys.argv[1])
 config = importlib.util.module_from_spec(spec)
 spec.loader.exec_module(config)
@@ -33,104 +33,83 @@ dtmeasure = config.dtmeasure
 cc2 = config.cc2
 beta = config.beta
 alpha2 = config.alpha2
-g = config.g
-#model_noise = config.model_noise
 U_relax = config.U_relax
 tau_rel = config.tau_rel
+g = config.g
+table_file_name = config.table_file_name
+model_noise = config.model_noise
+Nhr = config.Nhr
+Q_FUNC = config.Q_FUNC
+rMODNOISE = config.rMODNOISE
+hMODNOISE = config.hMODNOISE
+ass_freq= config.ass_freq
 
-# Q-matrix from pre-defined model noise
-#var_h = list(map(lambda x: np.repeat(x**2, Nk_fc), model_noise))[0]
-#var_u = list(map(lambda x: np.repeat(x**2, Nk_fc), model_noise))[1]
-#var_r = list(map(lambda x: np.repeat(x**2, Nk_fc), model_noise))[2]
-#Q = np.diag(np.concatenate((var_h,var_u,var_r)))
+# Q-matrix from pre-defined model noise (diagonal)
+def Q_predef():
+    var_h = list(map(lambda x: np.repeat(x**2, Nk_fc), model_noise))[0]
+    var_u = list(map(lambda x: np.repeat(x**2, Nk_fc), model_noise))[1]
+    var_r = list(map(lambda x: np.repeat(x**2, Nk_fc), model_noise))[2]
+    Q = np.diag(np.concatenate((var_h,var_u,var_r)))
 
+    return Q
 
-##################################################################
+#################################################################
 # Cycle over truth trajectory times.  Use each value as the initial condition
 # for both the truth (obtained from X_tr at the next timestep) and a
 # lower-resolution forecast.
-B = np.load(str(outdir + '/B_tr.npy')) # truth
-U_tr = np.load(str(outdir + '/U_tr_array_2xres_1h.npy')) # truth
-fc_grid = make_grid(Nk_fc, L) # forecast
-Kk_fc = fc_grid[0]
-xc = fc_grid[2]
-U_rel = U_relax(Neq,Nk_fc,L,V,xc,U_tr[:,0::dres,0])
-nsteps = Nmeas
-X = np.zeros([Neq * Nk_fc, nsteps])
-U = np.copy(U_tr[:, 0::dres, :]) # Sample truth at forecast resolution
-for T in range(nsteps):
-    tn = assim_time[T]
-    print(tn)
-    tmeasure = tn+1*dtmeasure
-    print(tmeasure)
-    U_fc = np.copy(U[:, :, T])
-    while tn < tmeasure:
-        dt = time_step(U_fc, Kk_fc, cfl_fc, cc2, beta, g) # compute stable time step
-        tn = tn + dt
-        if tn > tmeasure:
-            dt = dt - (tn - tmeasure) + 1e-12
-            tn = tmeasure + 1e-12
-        U_fc = step_forward_modRSW(U_fc, U_rel, dt, Neq, Nk_fc, Kk_fc, Ro, alpha2, Hc, Hr, cc2, beta, g, tau_rel)
-#        U_fc = step_forward_topog(U_fc, B, dt, tn, Neq, Nk_fc, Kk_fc, Hc, Hr, cc2, beta, alpha2, g)
-    X[:, T] = U[:, :, T+1].flatten() - U_fc.flatten()
-print("Means of proxy error components = ", \
-    np.mean(U[:, :, 1:(nsteps+1)]
-            - np.repeat(U_fc, nsteps).reshape(Neq, Nk_fc, nsteps), axis=(1, 2)))
+def Q_nhr():
+    B = np.load(str(outdir + '/B_tr.npy')) # truth
+    U_tr = np.load(str(outdir + '/U_tr_array_2xres_'+ass_freq+'.npy')) # truth
+    fc_grid = make_grid(Nk_fc, L) # forecast
+    Kk_fc = fc_grid[0]
+    xc = fc_grid[2]
+    nsteps = Nmeas
+    X = np.zeros([Neq * Nk_fc, nsteps])
+    U = np.copy(U_tr[:, 0::dres, :]) # Sample truth at forecast resolution
+    ### Relaxation solution ###
+    U_rel = U_relax(Neq,Nk_fc,L,V,xc,U[:,:,0])
+    for T in range(nsteps):
+        tn = assim_time[T]
+        tmeasure = tn+Nhr*dtmeasure
+        print('*** Integrating between time:', tn, ' and: ', tmeasure, ' ***')
+        U_fc = np.copy(U[:, :, T])
+        while tn < tmeasure:
+            dt = time_step(U_fc, Kk_fc, cfl_fc, cc2, beta, g) # compute stable time step
+            tn = tn + dt
+            if tn > tmeasure:
+                dt = dt - (tn - tmeasure) + 1e-12
+                tn = tmeasure + 1e-12
+#           U_fc = step_forward_modRSW(U_fc, U_rel, dt, Neq, Nk_fc, Kk_fc, Ro, alpha2, Hc, Hr, cc2, beta, g, tau_rel)
+            U_fc = step_forward_topog(U_fc, B, dt, tn, Neq, Nk_fc, Kk_fc, Hc, Hr, cc2, beta, alpha2, g)
+        print('*** Storing the error at time: ', tmeasure, ' ***')
+        X[:, T] = U[:, :, T+Nhr].flatten() - U_fc.flatten()
+    print("Means of proxy error components = ", np.mean(U[:, :, 1:(nsteps+1)] - np.repeat(U_fc, nsteps).reshape(Neq, Nk_fc, nsteps), axis=(1, 2)))
 
-# Compute the covariance matrix.
-Q = np.cov(X, bias=False)
+    # Compute the covariance matrix.
+    Q = np.cov(X, bias=False)
+    # Extrapolate diagonal of Q
+    Q_diag = np.array(Q.diagonal())
+    # Pose the covariance of r to 0
+    if(rMODNOISE==0):
+        Q_diag[2*Nk_fc:] = 0.0
+    if(hMODNOISE==0):
+        Q_diag[:Nk_fc] = 0.0
 
-#Pose the covariance of r to 0
-Q_diag = np.array(Q.diagonal())
-Q_diag[3*Nk_fc:] = 0.0
+    # Return a diagonal matrix
+    Q = np.diag(Q_diag)
 
-#Pose the covariance of u to 0
-#Q_diag = np.array(Q.diagonal())
-#Q_diag[Nk_fc:2*Nk_fc] = 0.0
+    return Q
 
-#Return a diagonal matrix
-Q = np.diag(Q_diag)
-#Q = np.diag(Q.diagonal())
+##################################################################
 
-# Recondition Q to have a maximum condition number of kappa following
-# Smith et al.: doi:10.1002/2017GL075534, modified to use singular value
-# decomposition.
-#u, s, vh = linalg.svd(Q)
-#print "Raw Q has 2-norm condition number = ", s[0] / s[-1]
+f_path_Q = str(outdir+'/Qmatrix.npy') 
 
-# Apply the required eigenvalue offset and reconstruct Q using U alone
-# to ensure positive semi-definiteness.
-#kappa = 1000
-#lam = (s[0] - kappa * s[-1]) / (kappa - 1)
-#print "Reconditioning singular value offset = ", lam
-#s += lam
-#Q = np.dot(np.dot(u, np.diag(s)), np.transpose(u))
-#print "Reconditioned Q has 2-norm condition number = ", s[0] / s[-1]
-
-# Check for positive semi-definiteness.
-#try:
-#    tmp = np.linalg.cholesky(Q)
-#except:
-#    raise
-
-#Q = np.diag(np.sum(X**2, axis=1) / nsteps)
-
-print(("Min. and max. variances", np.amin(Q.diagonal()), np.amax(Q.diagonal())))
-np.save(str(outdir + '/Qnew'), Q)
-
-# Plot the Q matrix.
-#fig = plt.figure(1)
-#ax = fig.add_subplot(111)
-#cax = ax.pcolormesh(Q, vmin=-0.02, vmax=0.02, cmap='RdBu_r')
-#fig.colorbar(cax)
-#fig.show()
-
-# Correlation matrix.
-#Qcorr = np.corrcoef(X, bias=False)
-#fig = plt.figure(2)
-#ax = fig.add_subplot(111)
-#cax = ax.pcolormesh(Qcorr, vmin=-1, vmax=1, cmap='RdBu_r')
-#fig.colorbar(cax)
-#fig.show()
-
-#raw_input() # Press 'Enter' to close
+# Load or generate Q matrix according to the choice made in the config file 
+try:
+    print(' *** Loading Q matrix *** ')
+    Q = np.load(f_path_Q)
+except:
+    print(' *** Generating Q matrix *** ')
+    Q = eval(Q_FUNC)
+    print(("Min. and max. variances", np.amin(Q.diagonal()), np.amax(Q.diagonal())))
+    np.save(str(outdir + '/Qmatrix'), Q)
